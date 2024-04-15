6ee196835a0328fd28c9905d2a28dd5bf0050d3e
blankblankblank1123 <166421152+blankblankblank1123@users.noreply.github.com>
2024-04-15 18:24:19 +0000

    giraffe image

--- javaScript/project.js
+++ javaScript/project.js
@@ -1,1 +1,28 @@
-console.log("Cheese")
+const app = new PIXI.Application({
+    height: 480,
+    width: 320
+});
+
+document.body.appendChild(app.view);
+
+// Loads image with the object/variable 'bunny'
+app.loader.add('giraffe', 'images/giraffe-400.png').load((loader, resources) => {
+    const giraffe = new PIXI.Sprite(resources.giraffe.texture);
+
+    // Sets position of image
+    giraffe.x = app.renderer.width / 2;
+    giraffe.y = app.renderer.height / 2;
+
+    // Rotates image around the center
+    giraffe.anchor.x = 0.5;
+    giraffe.anchor.y = 0.5;
+
+    app.stage.addChild(giraffe);
+
+    app.ticker.add(() => {
+        giraffe.rotation += 0.01;
+    });
+})
+
+// Logs an error to the console if an error occurs. 
+app.loader.onError.add((error) => console.error(error));
