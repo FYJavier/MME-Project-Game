a53cd8465660e3b14551e35422896a14ef91a140
Freya Javier <thesleepyunicorn63@gmail.com>
2024-04-16 19:28:51 -0500

    Imported tile sheet & added code to render one tile at a time

--- javaScript/project.js
+++ javaScript/project.js
@@ -3,11 +3,25 @@ const app = new PIXI.Application({
     width: 320
 });
 
+const tileSize = 16;
+
 document.body.appendChild(app.view);
 
 // Loads image with the object/variable 'bunny'
-app.loader.add('giraffe', 'images/giraffe-400.png').load((loader, resources) => {
-    const giraffe = new PIXI.Sprite(resources.giraffe.texture);
+app.loader.add('tileset', 'https://live.staticflickr.com/65535/53658268592_4995140e23_m.jpg').load((loader, resources) => {
+    
+
+    let tileTextures = [];
+    for (let i = 0; i < 7 * 11; i++) {
+        let x = i % 7;
+        let y = Math.floor(i / 7);
+        tileTextures[i] = new PIXI.Texture(
+            resources.tileset.texture, 
+            new PIXI.Rectangle(x * tileSize, x * tileSize, tileSize, tileSize)
+        );
+    }
+
+    const giraffe = new PIXI.Sprite(tileTextures[55]);
 
     // Sets position of image
     giraffe.x = app.renderer.width / 2;
